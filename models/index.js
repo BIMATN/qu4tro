96ee7418a030c1e003a4085095110b638881086b
Kui <kui_eugenio@hotmail.com>
2017-11-01 20:34:48 -0700

    updated db conn

--- models/index.js
+++ models/index.js
@@ -4,15 +4,23 @@ var fs        = require('fs');
 var path      = require('path');
 var Sequelize = require('sequelize');
 var basename  = path.basename(__filename);
-var env       = process.env.JAWSB_URL || 'test';
+var env       = process.env.JAWSB_URL;
 var config    = require(__dirname + '/../config/config.json')[env];
 var db        = {};
 
-if (config.use_env_variable) {
-  var sequelize = new Sequelize(process.env[config.use_env_variable]);
+// if (config.use_env_variable) {
+//   var sequelize = new Sequelize(process.env[config.use_env_variable]);
+// } else {
+//   var sequelize = new Sequelize(config.database, config.username, config.password, config);
+// }
+
+if (env) {
+  var sequelize = new Sequelize(env);
 } else {
   var sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
+
+
 
 fs
   .readdirSync(__dirname)
