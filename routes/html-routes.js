759a4321a1211216b0b0d223e9b0f8eea15cb1f5
Benjamin Rodriguez <bimatn@outlook.com>
2017-11-05 14:26:12 -0700

    simple cms with links to create and view tests

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -9,30 +9,6 @@ module.exports = function(app) {
     res.render("index-b");
   });
 
-  // when authenticating userName and password passed, if successful, route to home page else error
-  // use post here to "hide" values passed back to route.
-  app.post("/authenticate", function(req, res) {
-    var query = {};
-    if (req.body.userName && req.body.password) {
-      query = { user_name : req.body.userName,
-                password : req.body.password };
-    }
-
-    db.User.findAll({
-      where:query
-    }).then(function(data) {
-      console.log("data : " + JSON.stringify(data));
-      //console.log("user_name : " + data[0].user_name);      
-      if (!data || !data.length){
-        throw new Error("invalid login");
-      } else{
-       /* var filePath = path.join(__dirname,"../public/cms.html");
-        res.sendFile(filePath);*/
-        res.render("cms");
-      }
-    });
-  });
-
   // cms route loads cms.handlebars
   app.get("/cms", function(req, res) {
     /*res.sendFile(path.join(__dirname, "../public/cms.html"));*/
