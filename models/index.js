da9659b619274e2e3d1310ff0c9b3b79e06c107a
Kui <kui_eugenio@hotmail.com>
2017-11-05 20:14:27 -0700

    Revert "changing to JAWS_MAUVE_URL"
    This reverts commit cb092c70f729141a92a4288ed0c11f640f50f630.

--- models/index.js
+++ models/index.js
@@ -4,7 +4,7 @@ var fs        = require('fs');
 var path      = require('path');
 var Sequelize = require('sequelize');
 var basename  = path.basename(module.filename);
-var env       = process.env.JAWSDB_MAUVE_URL || 'development';
+var env       = process.env.JAWSB_URL || 'development';
 var config    = require(__dirname + '/../config/config.json')[env];
 var db        = {};
 
