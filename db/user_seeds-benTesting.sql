edfb08d3e40413a38e8531f6ec274102ca5ca226
Benjamin Rodriguez <bimatn@outlook.com>
2017-11-06 17:21:21 -0700

    working log in username passing
    still need to store in local storage...?

--- db/user_seeds-benTesting.sql
+++
@@ -1,3 +0,0 @@
-SELECT * FROM quatro_db.users;
-
-INSERT INTO quatro_db.users (user_name, password, role)VALUES ("bob", "bob", "developer")
--- routes/html-routes.js
+++ routes/html-routes.js
@@ -2,10 +2,9 @@ var path = require('path');
 var db = require("../models");
 
 module.exports = function(app) {
-
   // goes to home page when accessing home page of site
   app.get("/", function(req, res) {
-    res.render("index-b");
+    res.render("index");
   });
 
   // cms route loads cms.handlebars
@@ -47,9 +46,10 @@ module.exports = function(app) {
     // res.sendFile(path.join(__dirname, "../public/apiInfo.html"));
     res.render("apiInfo");
   });
-  // when authenticating userName and password passed, if successful, route to home page else error
+  // when authenticating userName and password passed, if successful, route to cms page else error
   // use post here to "hide" values passed back to route.
   app.post("/authenticate", function(req, res) {
+    console.log(req.body);
     var query = {};
     if (req.body.userName && req.body.password) {
       query = { user_name : req.body.userName,
@@ -65,14 +65,14 @@ module.exports = function(app) {
       } else{
        /* var filePath = path.join(__dirname,"../public/cms.html");
         res.sendFile(filePath);*/
-        res.render("cms",{user_name: data[0].user_name});
+        res.render("cms",{user_welcome: "Welcome, "+data[0].user_name});
+        //Need to save username or user id to session storage so we can keep them logged in while the tab is open
       }
     });
   });
   // route loads quiz.handlebars
   app.post("/quiz", function(req, res) {
     var query = {};
-
     if (req.body.quizId) {
       query.QuizId = req.body.quizId;
       db.Question.findAll({
@@ -86,6 +86,4 @@ module.exports = function(app) {
       res.render(req.body.pageName, {quizIdError: "Incorrect Quiz ID. Please try again"});
     }
   });  
-};
-
 };
