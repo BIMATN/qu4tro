c20ef9749fdf4b2ab7fe5c5e7e6ea8ee2c0f19b7
Benjamin Rodriguez <bimatn@outlook.com>
2017-11-08 15:35:33 -0700

    linking js files and adding register page

--- public/assets/js/about.js
+++ public/assets/js/about.js
@@ -1,6 +1,13 @@
 $(function(){
-	$("#navbarDropdown").text("Welcome, "+sessionStorage.userName);
-	$("#logOut").on("click", function(){
-		sessionStorage.clear();
-	})
+	if(sessionStorage.userName){
+		$("#navbarDropdown").text("Welcome, "+sessionStorage.userName);
+		$("#logOut").on("click", function(){
+			sessionStorage.clear();
+		});
+	}
+	else{
+		$("#navbarDropdown").text("Welcome, Guest");
+		$(".dropdown-item").text("Register");
+		$(".dropdown-item").attr("href", "/register");
+	};
 })
